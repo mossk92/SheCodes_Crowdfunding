7fde45d0718ca274b77b5a81bf899600babf9fa6
mossk92 <86653337+mossk92@users.noreply.github.com>
2021-11-24 20:38:59 +1000

    Added tokens to models

--- crowdmentor/users/models.py
+++ crowdmentor/users/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from datetime import date
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -12,3 +16,8 @@ class CustomUser(AbstractUser):
     def __str__(self):
         #display as just the username (no {})
         return self.username
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
