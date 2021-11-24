ad5c514f8c3db3cdd7f7582351c370b8e77d6743
mossk92 <86653337+mossk92@users.noreply.github.com>
2021-11-24 20:44:31 +1000

    Updated URLS with token link

--- crowdmentor/users/urls.py
+++ crowdmentor/users/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
      path('users/', views.CustomUserList.as_view()),
      path('users/<int:pk>/', views.CustomUserDetail.as_view()),
+     path('api-token-auth/', obtain_auth_token)
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
